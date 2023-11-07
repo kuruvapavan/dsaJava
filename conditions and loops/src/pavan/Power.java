2b6940b3a9318055d765d0c4ea9cc9dc46d1757b
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2023-11-07 15:31:32 -0500

    new code

--- conditions and loops/src/pavan/Power.java
+++ conditions and loops/src/pavan/Power.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 
 public class Power {
     public static void main(String[] args) {
+//        int base=1;
+//        int exponent=1;
+//        double num;
+//        System.out.println("enter the base :" +base);
+//        Scanner input = new Scanner(System.in);
+//        base=input.nextInt();
+//        System.out.println("Enter the exponent:"+exponent);
+//        exponent=input.nextInt();
+//
+//        num=Math.pow(base,exponent);
+//        System.out.println(num);
+
         int base=1;
         int exponent=1;
         double num;
-        System.out.println("enter the base :" +base);
-        Scanner input = new Scanner(System.in);
-        base=input.nextInt();
-        System.out.println("Enter the exponent:"+exponent);
-        exponent=input.nextInt();
+        System.out.println("Enter the base: ");
+        Scanner in = new Scanner(System.in);
+        base=in.nextInt();
+        System.out.println("Enter the exponent: ");
+        exponent=in.nextInt();
 
-        num=Math.pow(base,exponent);
+         num=Math.pow(base,exponent);
         System.out.println(num);
     }
 }
