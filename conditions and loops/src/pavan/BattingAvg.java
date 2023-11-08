52b6448c20ac2e5136feef20c14aa554ce60c2b6
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2023-11-08 14:00:20 -0500

    new codegg

--- conditions and loops/src/pavan/BattingAvg.java
+++ conditions and loops/src/pavan/BattingAvg.java
@@ -6,17 +6,29 @@ public class BattingAvg {
 
 
     public static void main(String[] args) {
+//        double innings;
+//        double runs=0;
+//        double average=0;
+//        System.out.println("Enter the no of runs scored :");
+//        Scanner input =  new Scanner(System.in);
+//        runs=input.nextDouble();
+//        System.out.println("Enter number innings not out:");
+//        innings=input.nextDouble();
+//        if (innings>0 && runs>0){
+//            average=runs/innings;
+//        }
+//        System.out.println("Battinf average :" +average);
         double innings;
         double runs=0;
-        double average=0;
-        System.out.println("Enter the no of runs scored :");
-        Scanner input =  new Scanner(System.in);
-        runs=input.nextDouble();
-        System.out.println("Enter number innings not out:");
-        innings=input.nextDouble();
-        if (innings>0 && runs>0){
-            average=runs/innings;
+        double avg=0;
+        System.out.println("Enter No of runs Scored: ");
+        Scanner in =new Scanner(System.in);
+        runs=in.nextDouble();
+        System.out.println("Enter no of notout innings: ");
+        innings=in.nextDouble();
+        if (runs>0 && innings>0){
+            avg=runs/innings;
         }
-        System.out.println("Battinf average :" +average);
+        System.out.println("average is " +avg);
     }
 }
--- conditions and loops/src/pavan/CGPA.java
+++ conditions and loops/src/pavan/CGPA.java
@@ -4,7 +4,53 @@ import java.util.Scanner;
 
 public class CGPA {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter no of subjects: ");
+        int numOfSubs = in.nextInt();
+        double totalCredits = 0;
+        double totalGradePoints = 0;
 
+        for (int i = 1; i <= numOfSubs; i++) {
+            System.out.println("Enter the credits for subject" + i + ": ");
+            int credits = in.nextInt();
+            System.out.println("Enter the grade for subject " + i + ": ");
+            String grade = in.next();
 
+            double gradePoint = caluclateGradePoints(grade);
+
+            totalCredits += credits;
+            totalGradePoints += (credits * gradePoint);
+        }
+        double cgpa = totalGradePoints / totalCredits;
+        System.out.println("CGPA is: " + cgpa);
+
+    }
+    public static double caluclateGradePoints(String grade){
+        switch (grade){
+            case "A+":
+                return 4.0;
+            case  "A":
+                return 4.0;
+            case "A-":
+                return 3.7;
+            case "B+":
+                return 3.3;
+            case "B":
+                return 3.0;
+            case "B-":
+                return 2.7;
+            case "C+":
+                return 2.3;
+            case "C":
+                return 2.0;
+            case "C-":
+                return 1.7;
+            case "D":
+                return 1.0;
+            case "F":
+                return 0.0;
+            default:
+                return 0.0;
+        }
     }
 }
