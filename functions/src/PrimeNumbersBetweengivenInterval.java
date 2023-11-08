e3ff68067c6016e78bf825d7fc5cc8462b25b8a2
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2023-11-08 15:41:35 -0500

    code

---
+++ functions/src/PrimeNumbersBetweengivenInterval.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeNumbersBetweengivenInterval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, i, j, flag;
+        System.out.printf("Enter lower bound of the interval: ");
+        a = sc.nextInt();
+        System.out.printf("\nEnter upper bound of the interval: ");
+        b = sc.nextInt();
+        System.out.printf("\nPrime numbers between %d and %d are: ", a, b);
+
+        for (i = a; i <= b; i++) {
+            if (i == 1 || i == 0)
+                continue;
+            flag = 1;
+
+            for (j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            // flag = 1 means i is prime
+            // and flag = 0 means i is not prime
+            if (flag == 1)
+                System.out.println(i);
+        }
+    }
+}
