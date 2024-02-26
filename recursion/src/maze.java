d0c52ae5b2f06b6deac2a1acb423492ab23f5733
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2024-02-26 15:34:09 -0500

    code

--- recursion/src/maze.java
+++ recursion/src/maze.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class maze {
     public static void main(String[] args) {
         System.out.println(count(3,3)); // Prints the count of paths
         mazePath("", 3, 3); // Prints paths without indices
         System.out.println(count(3,3)); // Prints the count of paths again (this line is redundant and can be removed)
         mazePath("", 3, 3, 1, 1); // Prints paths with indices
+        System.out.println(mazeDiagonal("",3,3));
     }
 
     // Method to count the number of paths
@@ -42,5 +45,25 @@ public class maze {
         if (currentCol < c){
             mazePath(p + "(" + currentRow + "," + currentCol + ")" + "->R", r, c, currentRow, currentCol + 1);
         }
+    }
+
+    static ArrayList<String> mazeDiagonal(String p, int r, int c){
+        if (r==1 && c==1){
+            System.out.println(p);
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r>1 &&  c>1){
+            list.addAll(mazeDiagonal(p+'D',r-1,c-1));
+        }
+        if (r>1){
+            list.addAll(mazeDiagonal(p+'V',r-1,c));
+        }
+        if (c>1){
+            list.addAll(mazeDiagonal(p+'H',r,c-1));
+        }
+        return list;
     }
 }
